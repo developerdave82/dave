839239517588f83e539dcacb73e14924ed6660f0
developerdave82@yahoo.com <developerdave82@yahoo.com>
2024-02-16 19:20:57 +0000

    made CLEAN tables

---
+++ models/clean_data/salesforce/clean_sf_branch.sql
@@ -0,0 +1,22 @@
+WITH 
+
+raw AS (
+
+    SELECT *
+    FROM {{ source ('snowflake', 'branch') }}
+
+)
+
+clean AS (
+    SELECT
+        REGION
+        ,AREA
+        ,BRANCH 
+        ,CITY
+        ,STATE
+        ,ZIP
+    FROM raw
+)
+
+SELECT *
+FROM clean
